182a43dd0a075779102702ca557ea509e65e6205
srt1104 <ragitss1104@gmail.com>
2023-08-16 08:45:23 +0000

    auth ms | add current-user logic

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,20 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
+
+router.get("/api/users/current-user", (req, res) => {
+  console.log(req.session);
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    return res.send({ currentUser: null });
+  }
+});
 
 export { router as currentUserRouter };
