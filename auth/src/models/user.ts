4a5dd83ed2d6aa6c07df5b329b53a40deced706b
srt1104 <ragitss1104@gmail.com>
2023-08-16 03:54:14 +0000

    auth ms | add signup logic

---
+++ auth/src/models/user.ts
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+import { Password } from "../services/password";
+
+// Describes properties required to create a new `User`
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// Describes properties that a `User` Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+}
+
+// Describes properties that a `User` Model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.password;
+        delete ret.__v;
+
+        delete ret._id;
+        ret.id = doc.id;
+      },
+    },
+  }
+);
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashedPassword = await Password.toHash(this.get("password"));
+    this.set("password", hashedPassword);
+  }
+
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<UserDoc, UserModel>("User", userSchema);
+
+export { User };
