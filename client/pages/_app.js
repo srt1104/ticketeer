2ae394a4b105fd77aa7308a440bf01608021af08
srt1104 <ragitss1104@gmail.com>
2023-08-26 06:33:10 +0000

    client | add auth pages

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,30 @@
 import "bootstrap/dist/css/bootstrap.css";
 
-const Default = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import buildClient from "../api/build-client";
+import Header from "../components/header";
+
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
 
-export default Default;
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/current-user");
+
+  let pageProps;
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return {
+    pageProps,
+    currentUser: data.currentUser,
+  };
+};
+
+export default AppComponent;
