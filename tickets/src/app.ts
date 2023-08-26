18b3aa09813f25e21bd5cf87cdb8220fe6a010cc
srt1104 <ragitss1104@gmail.com>
2023-08-26 09:40:14 +0000

    create tickets ms boilerplate and k8 objects

---
+++ tickets/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError } from "@srt1104-tickets/common";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({ signed: false, secure: process.env.NODE_ENV !== "test" })
+);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
