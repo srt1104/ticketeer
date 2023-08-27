2f732513deba59c9ebb7e1777a94ae5904208a5c
srt1104 <ragitss1104@gmail.com>
2023-08-27 01:58:56 +0000

    tickets ms | update signin test function

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -33,7 +33,7 @@ afterAll(async () => {
 global.signin = () => {
   // Build a JWT payload - { id, email }
   const payload = {
-    id: "dummy_id",
+    id: new mongoose.Types.ObjectId().toHexString(),
     email: "dummy@email.com",
   };
 
