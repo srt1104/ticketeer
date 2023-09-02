fea348ee71ddb0096405423116da43012b495e53
srt1104 <ragitss1104@gmail.com>
2023-09-02 12:04:02 +0000

    add create-order logic

---
+++ orders/src/models/ticket.ts
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@srt1104-tickets/common";
+
+import { Order } from "./order";
+
+interface TicketAttrs {
+  title: string;
+  price: number;
+}
+
+export interface TicketDoc extends mongoose.Document {
+  title: string;
+  price: number;
+  isReserved(): Promise<boolean>;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        delete ret.__v;
+        delete ret._id;
+
+        ret.id = doc.id;
+      },
+    },
+  }
+);
+
+ticketSchema.methods.isReserved = async function () {
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  });
+
+  return existingOrder !== null;
+};
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
---
+++ orders/src/routes/__test__/create-order.test.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { OrderStatus } from "@srt1104-tickets/common";
+
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+
+it("returns an error if the ticket does not exist", async () => {
+  const ticketId = new mongoose.Types.ObjectId();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", signin())
+    .send({ ticketId })
+    .expect(404);
+});
+
+it("returns an error if the ticket is already reserved", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const order = Order.build({
+    ticket,
+    userId: "valid_user_id",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", signin())
+    .send({ ticketId: ticket.id })
+    .expect(400);
+});
+
+it("returns a ticket", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+});
