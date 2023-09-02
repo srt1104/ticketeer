4aa244708dc4f0492a2f6cf2945a47caf9d15330
srt1104 <ragitss1104@gmail.com>
2023-09-02 13:26:53 +0000

    add delete-order logic

--- orders/src/routes/delete-order.ts
+++ orders/src/routes/delete-order.ts
@@ -1,9 +1,33 @@
 import express, { Request, Response } from "express";
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+  requireAuth,
+} from "@srt1104-tickets/common";
+
+import { Order } from "../models/order";
 
 const router = express.Router();
 
-router.delete("/api/orders/:id", async (req: Request, res: Response) => {
-  res.send("delete-orders");
-});
+router.delete(
+  "/api/orders/:id",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.id);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
