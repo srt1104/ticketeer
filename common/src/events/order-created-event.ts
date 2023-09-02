86023caf14e4207070ecddcef944ed47835470c9
srt1104 <ragitss1104@gmail.com>
2023-09-02 15:07:11 +0000

    common module | add order-created and order-cancelled events

---
+++ common/src/events/order-created-event.ts
@@ -0,0 +1,14 @@
+import { Subjects } from "./subjects";
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    userId: string;
+    expiresAt: string;
+    ticket: {
+      id: string;
+      price: number;
+    };
+  };
+}
