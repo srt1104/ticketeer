63f592f8ded85d9a4f35616fcf66e6198488c243
srt1104 <ragitss1104@gmail.com>
2023-09-09 13:33:17 +0000

    client | order expiration

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,5 +1,34 @@
-const ViewOrder = () => {
-  return <div>ViewOrder</div>;
+import { useState, useEffect } from "react";
+
+const ViewOrder = ({ order }) => {
+  const [timeLeft, setTimeLeft] = useState(0);
+
+  useEffect(() => {
+    const findTimeLeft = () => {
+      const msLeft = new Date(order.expiresAt) - new Date();
+      setTimeLeft(Math.floor(msLeft / 1000));
+    };
+
+    findTimeLeft();
+    const timerId = setInterval(findTimeLeft, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  if (timeLeft < 0) {
+    return <div>Order Expired</div>;
+  }
+
+  return <div>Time left to pay: {timeLeft} seconds</div>;
+};
+
+ViewOrder.getInitialProps = async (context, client) => {
+  const { orderId } = context.query;
+  const { data } = await client.get(`/api/orders/${orderId}`);
+
+  return { order: data };
 };
 
 export default ViewOrder;
